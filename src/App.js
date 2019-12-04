ce3c50948be218ab2d6f6974059ebf0c85d23f76
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-04 17:11:56 -0600

    agregando formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <h1>Administrador de Pacientes</h1>
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+              <Formulario />
+          </div>
+          <div className="one-half column">
+              2
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Formulario = () => {
+    return ( 
+        <h1>Desde formulario</h1>
+     );
+}
+ 
+export default Formulario;
