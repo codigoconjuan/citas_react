7caaf7a616c4cb9147459e3430c18fb0f218483a
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-04 18:53:53 -0600

    agregando uuid

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import uuid from 'uuid/v4';
 
 const Formulario = () => {
 
@@ -34,8 +35,11 @@ const Formulario = () => {
             actualizarError(true);
             return;
         }
+        // Eliminar el mensaje previo 
+        actualizarError(false);
 
         // Asignar un ID
+        cita.id = uuid();
 
         // Crear la cita
 
