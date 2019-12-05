35aa59a5d3e94ba9e6efc39e31b50bcd2c2492b2
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-04 19:22:52 -0600

    eliminando citas

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
     guardarCitas([ ...citas, cita ]);
   }
 
+  // Función que elimina una cita por su id
+  const eliminarCita = id => {
+     const nuevasCitas = citas.filter(cita => cita.id !== id );
+     guardarCitas(nuevasCitas);
+  }
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -30,6 +36,7 @@ function App() {
                 <Cita
                   key={cita.id}
                   cita={cita}
+                  eliminarCita={eliminarCita}
                 />
               ))}
           </div>
--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Cita = ({cita}) => ( 
+const Cita = ({cita, eliminarCita}) => ( 
     <div className="cita">
         <p>Mascota: <span>{cita.mascota}</span> </p>
         <p>Dueño: <span>{cita.propietario}</span> </p>
@@ -8,6 +8,10 @@ const Cita = ({cita}) => (
         <p>Hora: <span>{cita.hora}</span> </p>
         <p>Sintomas: <span>{cita.sintomas}</span> </p>
 
+        <button
+            className="button eliminar u-full-width"
+            onClick={ () => eliminarCita(cita.id)  }
+        >Eliminar &times;</button>
     </div>
 );
  
