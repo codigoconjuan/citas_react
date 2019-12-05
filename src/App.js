01b944be71a136aa07831d8b6d8a426dc1c212e9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-04 19:02:58 -0600

    agregando la cita al state de citas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  // Arreglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+  // Función que tome las citas actuales y agregue la nueva
+  const crearCita = cita => {
+    guardarCitas([ ...citas, cita ]);
+  }
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -9,7 +18,9 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="one-half column">
-              <Formulario />
+              <Formulario 
+                crearCita={crearCita}
+              />
           </div>
           <div className="one-half column">
               2
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import uuid from 'uuid/v4';
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 
     // Crear State de Citas
     const [cita, actualizarCita] = useState({
@@ -12,8 +12,6 @@ const Formulario = () => {
         sintomas: ''
     });
     const [ error, actualizarError ] = useState(false)
-
-
 
     // Función que se ejecuta cada que el usuario escribe en un input
     const actualizarState = e => {
@@ -42,6 +40,7 @@ const Formulario = () => {
         cita.id = uuid();
 
         // Crear la cita
+        crearCita(cita);
 
         // Reiniciar el form
     }
