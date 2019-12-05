b2bed87f41f8b5204a092f7fbbac1af3a85af3f9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-04 19:57:31 -0600

    agregando localstorage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
-
 function App() {
 
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
   // Arreglo de citas
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
 
   // Use Effect para realizar ciertas operaciones cuando el state cambia
   useEffect( () => {
-    console.log('Documento listo o algo paso con las citas');
+      if(citasIniciales) {
+        localStorage.setItem('citas', JSON.stringify(citas))
+      } else {
+        localStorage.setItem('citas', JSON.stringify([]));
+      }
   }, [citas] );
 
   // Función que tome las citas actuales y agregue la nueva
