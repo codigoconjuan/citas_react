fddee7d799fe40665365b329094b143ed27354c6
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-04 19:15:57 -0600

    listando las citas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
 
 function App() {
 
@@ -23,7 +25,13 @@ function App() {
               />
           </div>
           <div className="one-half column">
-              2
+              <h2>Administra tus citas</h2>
+              {citas.map(cita => (
+                <Cita
+                  key={cita.id}
+                  cita={cita}
+                />
+              ))}
           </div>
         </div>
       </div>
---
+++ src/components/Cita.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cita = ({cita}) => ( 
+    <div className="cita">
+        <p>Mascota: <span>{cita.mascota}</span> </p>
+        <p>Dueño: <span>{cita.propietario}</span> </p>
+        <p>Fecha: <span>{cita.fecha}</span> </p>
+        <p>Hora: <span>{cita.hora}</span> </p>
+        <p>Sintomas: <span>{cita.sintomas}</span> </p>
+
+    </div>
+);
+ 
+export default Cita;
